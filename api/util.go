599969fc9e1961d299e156362aca4c29af894d59
Nguyễn Cát Tiên <tiennc@gtv.vn>
2022-11-13 23:27:51 +0700

    add error code

--- api/util.go
+++ api/util.go
@@ -1,6 +1,9 @@
 package api
 
-import "github.com/labstack/echo/v4"
+import (
+	"context"
+	"github.com/labstack/echo/v4"
+)
 
 func GetContextDataString(ctx echo.Context, key string, defaultValues ...string) string {
 	defaultValue := ""
@@ -20,4 +23,13 @@ func GetContextDataString(ctx echo.Context, key string, defaultValues ...string)
 
 func SetContextDataString(ctx echo.Context, key string, value interface{}) {
 	ctx.Set(key, value)
+}
+
+func GetRequestContext(e echo.Context) context.Context {
+	ctx := e.Request().Context()
+	if ctx == nil {
+		ctx = context.Background()
+	}
+
+	return ctx
 }
